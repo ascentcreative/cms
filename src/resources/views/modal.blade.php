6012c8be735e2cf997545d9979ef41b3bae3842a
Kiersm <kieran@ascent-creative.co.uk>
2021-03-17 13:17:43 +0000

    Positioning on screen

--- src/resources/views/modal.blade.php
+++ src/resources/views/modal.blade.php
@@ -1,5 +1,5 @@
 <div class="modal fade text-left" id="ajaxModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered {{$modalSize ?? ''}}" role="document">
+    <div class="modal-dialog {{-- modal-dialog-centered --}} {{$modalSize ?? ''}}" role="document">
       <div class="modal-content">
 
         @if($modalShowHeader ?? false)
