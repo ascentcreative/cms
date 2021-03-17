789f753d765d61db3d2af46b5f17c3bfed85386f
Kiersm <kieran@ascent-creative.co.uk>
2021-03-17 12:06:48 +0000

    Variables / Options for header/footer display

--- src/resources/views/modal.blade.php
+++ src/resources/views/modal.blade.php
@@ -1,28 +1,36 @@
-
-
 <div class="modal fade text-left" id="ajaxModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-dialog modal-dialog-centered {{$modalSize ?? ''}}" role="document">
       <div class="modal-content">
-        {{-- <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">@yield('modalTitle')</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div> --}}
+
+        @if($modalShowHeader ?? false)
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">@yield('modalTitle')</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+        @endif
+
         <div class="modal-body">
           
             @yield('modalContent')
            
         </div>
-        {{-- <div class="modal-footer">
-            
-            @yield('modalButtons')
 
-            @sectionMissing('modalButtons')
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            @endif
-         
-        </div> --}}
+       
+        @if( $modalShowFooter ?? false )
+           <div class="modal-footer">
+                
+                @yield('modalButtons')
+
+                @sectionMissing('modalButtons')
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                @endif
+            
+          </div>
+        @endif
+    
+
       </div>
     </div>
   </div>
