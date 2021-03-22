9e35f26ee7121d1219061a4e185b182b7804b15d
Kiersm <kieran@ascent-creative.co.uk>
2021-03-22 00:28:32 +0000

    extender blade paths

--- src/config/cms.php
+++ src/config/cms.php
@@ -5,4 +5,9 @@ return [
     'wrapper_blade' => env('CMS_WRAPPER_BLADE', 'base'),
     'wrapper_blade_section' => env('CMS_WRAPPER_BLADE_SECTION', 'pagebody'),
  
+
+    // list of paths to try for Model Extender trait blades. Will be checked in the order specified.
+    'traitbladepaths' => ['admin.trait', 'cms::trait']
+
+
 ];
