b118dd9b9efb3eb373c850c8fdfc204406646211
Kiersm <kieran@ascent-creative.co.uk>
2021-03-22 00:27:55 +0000

    User Controller

---
+++ src/Controllers/Admin/UserController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AscentCreative\CMS\Controllers\Admin;
+
+use AscentCreative\CMS\Controllers\AdminBaseController;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserController extends AdminBaseController
+{
+
+    static $modelClass = 'App\Models\User';
+    static $bladePath = "cms::admin.users";
+
+
+    public function rules($request, $model=null) {
+
+       return [
+            'title' => 'required',
+        ]; 
+
+    }
+
+
+    public function autocomplete(Request $request, string $term) {
+
+        echo $term;
+
+    }
+
+
+
+}
