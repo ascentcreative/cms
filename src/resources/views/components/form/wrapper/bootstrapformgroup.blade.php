dec80f2bceb4ff4505a47eadd503ffa5ab98dc12
Kiersm <kieran@ascent-creative.co.uk>
2021-03-26 16:53:25 +0000

    Hide SLot element if empty

--- src/resources/views/components/form/wrapper/bootstrapformgroup.blade.php
+++ src/resources/views/components/form/wrapper/bootstrapformgroup.blade.php
@@ -2,9 +2,11 @@
     <label for="@yield('name')" class="col-2 col-form-label">@yield('label'):</label>
     <div class="col">
         @yield('element')
-        <small class="form-text text-muted">
-            {{ $slot }}
-        </small>
+        @if($slot)
+            <small class="form-text text-muted">
+                {{ $slot }}
+            </small>
+        @endif
         @if($errors->first($name))
             <small class="validation-error alert alert-danger form-text" role="alert">
                 {{ $errors->first($name) }}
