715a07eca2ac4e109fa37b774fca4219157a3045
Kiersm <kieran@ascent-creative.co.uk>
2021-03-29 11:37:07 +0100

    Removal of AYS script - now global

--- src/resources/views/admin/menus/edit.blade.php
+++ src/resources/views/admin/menus/edit.blade.php
@@ -1,27 +1,6 @@
 @extends('cms::admin.base.edit')
 
 
-@push('scripts')
-    @script('/vendor/ascent/cms/jquery/areyousure/jquery.are-you-sure.js')
-    @script('/vendor/ascent/cms/jquery/areyousure/ays-beforeunload-shim.js')
-
-    <script language="javascript">
-        $(document).ready(function() {
-            $('#frm_edit').areYouSure( {'message':'Your edits have not been saved!'} );
-	
-        });
-    </script>
-
-    <script>
-
-        $(document).ready(function() {
-            $('#myTab li:first-child a').tab('show');
-        });
-
-    </script>
-
-@endpush
-
 
 @section('editform')
    
--- src/resources/views/admin/users/edit.blade.php
+++ src/resources/views/admin/users/edit.blade.php
@@ -1,28 +1,6 @@
 @extends('cms::admin.base.edit')
 
 
-@push('scripts')
-    @script('/vendor/ascent/cms/jquery/areyousure/jquery.are-you-sure.js')
-    @script('/vendor/ascent/cms/jquery/areyousure/ays-beforeunload-shim.js')
-
-    <script language="javascript">
-        $(document).ready(function() {
-            $('#frm_edit').areYouSure( {'message':'Your edits have not been saved!'} );
-	
-        });
-    </script>
-
-    <script>
-
-        $(document).ready(function() {
-            $('#myTab li:first-child a').tab('show');
-        });
-
-    </script>
-
-@endpush
-
-
 @section('editform')
 
 <div class="cms-screenblock cms-screenblock-main bg-white rounded shadow" style="">
