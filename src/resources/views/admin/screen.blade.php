1ab7edae6d6ec33e47f0719d48b661515080284a
Kiersm <kieran@ascent-creative.co.uk>
2021-03-30 16:38:35 +0100

    Layout Adjust

--- src/resources/views/admin/screen.blade.php
+++ src/resources/views/admin/screen.blade.php
@@ -7,7 +7,7 @@
 
             @yield('screen-start')
 
-                <div style="display: flex; justify-content: space-between">
+                <div style="display: flex; justify-content: space-between; margin-bottom: 10px;">
                     <h1>@yield('screentitle')</h1>
 
                     {{-- <div class="cms-headbar " style="">background: white; border-radius: 10px; box-shadow: 0, 0.125em, 2em, rgba(0,0,0,0.8); padding: 2rem; margin-bottom: 1em"> --}}
@@ -15,9 +15,6 @@
                     {{-- </div> --}}
 
                 </div>
-
-                
-
                 
                 @yield('screen')
                 
