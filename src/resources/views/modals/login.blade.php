2bc535c5272a7715dff978d8e8d2f3449b05b8f9
Kiersm <kieran@ascent-creative.co.uk>
2021-03-31 18:12:01 +0100

    Moving Script blocks

--- src/resources/views/modals/login.blade.php
+++ src/resources/views/modals/login.blade.php
@@ -1,13 +1,18 @@
 @extends('cms::modal')
 
+{{-- @php
+    $modalSize = 'modal-lg';
+    
+@endphp --}}
+
 @section('modalContent')
 
     @includeFirst(['auth.loginform', 'cms::auth.loginform'], ['intended'=>request()->intended])
 
-    <SCRIPT>
-
-        $('A#btn-register').addClass('modal-link').attr('href', '/modal/cms::modals.register?intended=' + $('#form_login input[name="intended"]').val());
-
-    </SCRIPT>
-
 @endsection
+
+@push('scripts')
+<SCRIPT>
+    $('A#btn-register').addClass('modal-link').attr('href', '/modal/cms::modals.register?intended=' + $('#form_login input[name="intended"]').val());
+</SCRIPT>
+@endpush
--- src/resources/views/modals/register.blade.php
+++ src/resources/views/modals/register.blade.php
@@ -1,61 +1,68 @@
 @extends('cms::modal')
+{{-- 
+@php
+    $modalSize = 'modal-lg';
+    
+@endphp --}}
 
 @section('modalContent')
 
     @includeFirst(['auth.registerform', 'cms::auth.registerform'], ['intended'=>request()->intended])
 
-     {{-- Prevent the form submittting normally - need to ajax it --}}
-     <SCRIPT>
-
-         $(document).ready(function () {
-            $('A#btn-login').addClass('modal-link').attr('href', '/modal/cms::modals.login?intended=' + $('#form_register input[name="intended"]').val());
-         });
-
-
-        // $('#form_register').submit(function() {
-
-        //     $('.validation-error').remove();
-
-        //     $.ajax({
-        //         type: 'POST',
-        //         url: '/register', 
-        //          headers: {
-        //             'Accept' : "application/json"
-        //          },
-        //         data: $('#form_register').serialize(),
-        //         statusCode: {
-        //             302: function(data, xhr, request) {
-
-        //                 console.log(data);
-        //                 console.log(xhr);
-        //                 console.log(request);
-
-        //                 $('body').modalLink({
-        //                     target: data.responseJSON
-        //                 });
-
-        //                 //$('.modal').modal('hide');
-
-        //             },
-        //             422: function(data, xhr, request) {
-        //                 for(name in data.responseJSON.errors) { 
-
-        //                     console.log(name + " --- " + data.responseJSON.errors[name]);
-
-        //                     $('INPUT[name="' + name + '"]').parents('.element-wrapper').append('<small class="validation-error alert alert-danger form-text" role="alert">' +
-        //                         data.responseJSON.errors[name] + 
-        //                     '</small>');
-
-        //                 }
-        //             }
-        //         }
-        //     });
-            
-        //     return false;
-
-         
-        // });
-
-    </SCRIPT>
-
 @endsection
+
+@push('scripts')
+<SCRIPT>
+
+    $(document).ready(function () {
+       $('A#btn-login').addClass('modal-link').attr('href', '/modal/cms::modals.login?intended=' + $('#form_register input[name="intended"]').val());
+       $('A#terms-link').removeClass('modal-link').attr('target', '_blank');
+    });
+    
+</SCRIPT>
+@endpush()
+{{-- OLD CODE FOR REFERENCE - now moved to modalLink.js --}}
+    {{--
+   // $('#form_register').submit(function() {
+
+   //     $('.validation-error').remove();
+
+   //     $.ajax({
+   //         type: 'POST',
+   //         url: '/register', 
+   //          headers: {
+   //             'Accept' : "application/json"
+   //          },
+   //         data: $('#form_register').serialize(),
+   //         statusCode: {
+   //             302: function(data, xhr, request) {
+
+   //                 console.log(data);
+   //                 console.log(xhr);
+   //                 console.log(request);
+
+   //                 $('body').modalLink({
+   //                     target: data.responseJSON
+   //                 });
+
+   //                 //$('.modal').modal('hide');
+
+   //             },
+   //             422: function(data, xhr, request) {
+   //                 for(name in data.responseJSON.errors) { 
+
+   //                     console.log(name + " --- " + data.responseJSON.errors[name]);
+
+   //                     $('INPUT[name="' + name + '"]').parents('.element-wrapper').append('<small class="validation-error alert alert-danger form-text" role="alert">' +
+   //                         data.responseJSON.errors[name] + 
+   //                     '</small>');
+
+   //                 }
+   //             }
+   //         }
+   //     });
+       
+   //     return false;
+
+    
+   // }); --}}
