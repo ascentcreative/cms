cd758d0fe8b4586617d75f9d3dd35951e1c22281
Kiersm <kieran@ascent-creative.co.uk>
2021-04-02 13:04:49 +0100

    Undo

--- src/CMSServiceProvider.php
+++ src/CMSServiceProvider.php
@@ -6,7 +6,6 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Routing\Router;
 use Laravel\Fortify\Fortify;
-use Illuminate\Support\Facades\Schema;
 
 use AscentCreative\CMS\Helpers\HeadTitle;
 use AscentCreative\CMS\Helpers\AdminMenu;
@@ -33,8 +32,6 @@ class CMSServiceProvider extends ServiceProvider
 
   public function boot()
   {
-
-    Schema::defaultStringLength(191);
 
     $this->bootFortify();
 
