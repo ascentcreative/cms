a66310d82d8d646be5d0db51b73fc2cccd57e80f
Kiersm <kieran@ascent-creative.co.uk>
2021-04-07 16:38:15 +0100

    Page Title to HTML Head

--- src/Controllers/PageController.php
+++ src/Controllers/PageController.php
@@ -13,12 +13,14 @@ class PageController extends Controller
     /* even though the page model is defined here, the bindings swap the class at runtime */
     public function show(\AscentCreative\CMS\Models\Page $page) {
 
-    if (request()->wantsJson()) {
-        return view('cms::public.pages.modal')->withModel($page);
-    } else {
-        return view('cms::public.pages.show')->withModel($page);
-    }
+        headTitle()->add($page->title);
+
+        if (request()->wantsJson()) {
+            return view('cms::public.pages.modal')->withModel($page);
+        } else {
+            return view('cms::public.pages.show')->withModel($page);
+        }
     
-  }
+    }
 
 }
