5529492532078194f7caec8f5fa875b144da86fa
Kiersm <kieran@ascent-creative.co.uk>
2021-04-11 14:29:17 +0100

    Layout adjustments (fixing resize issues from padding)

--- resources/views/stack/block/row/edit.blade.php
+++ resources/views/stack/block/row/edit.blade.php
@@ -21,7 +21,7 @@ if(is_null($value) || $value == '') {
 
 @section('block-content')
 
-    <div class="container row items">
+    <div class="container row items" style="width: 100%">
 
         @if($value)
             @isset($value->items)
@@ -49,56 +49,60 @@ if(is_null($value) || $value == '') {
 @push('scripts')
 <script>
 
-     $('.items').sortable({
-         handle: '.blockitem-handle',
-         update: function(event, ui) {
-            // reapply field indexes to represent reordering
-            $('.items').each(function(rowidx) {
+    //  $('.items').sortable({
+    //      handle: '.blockitem-handle',
+    //      update: function(event, ui) {
+    //         // reapply field indexes to represent reordering
+    //         $('.items').each(function(rowidx) {
 
-                $(this).find('.blockitem').each(function(idx) {
+    //             $(this).find('.blockitem').each(function(idx) {
 
-                    $(this).find('INPUT, SELECT, TEXTAREA').each(function(fldidx) {
-                        //  console.log(idx + ' / ' + fldidx);
-                        var ary = $(this).attr('name').split(/(\[|\])/);
-                        ary[10] = idx;
-                        $(this).attr('name', ary.join(''));
+    //                 $(this).find('INPUT, SELECT, TEXTAREA').each(function(fldidx) {
+    //                     //  console.log(idx + ' / ' + fldidx);
+    //                     var ary = $(this).attr('name').split(/(\[|\])/);
+    //                     ary[10] = idx;
+    //                     $(this).attr('name', ary.join(''));
                         
-                    });
+    //                 });
 
-                });
+    //             });
 
-            });
+    //         });
 
-        }
+    //     }
 
-     });
+    //  });
 
 
-    $('.blockitem').resizable({
-        handles: 'e, w',
-        start: function(event, ui){
-            // sibTotalWidth = ui.originalSize.width + ui.originalElement.next().outerWidth();
+    // $('.blockitem').resizable({
+    //     handles: 'e, w',
+    //     start: function(event, ui){
+    //         // sibTotalWidth = ui.originalSize.width + ui.originalElement.next().outerWidth();
+    //         console.log(ui.size);
+    //         // set the grid correctly - allows for window to be resized bewteen...
+    //         $(ui.element).resizable('option', 'grid', [ $(ui.element).parents('.items').width() / 4, 0 ]);
             
-            // set the grid correctly - allows for window to be resized bewteen...
-            $(ui.element).resizable('option', 'grid', $(ui.element).parents('.items').width() / 12);
+    //         // swap all col-* classes for %ge widths and drop col widths into data objects
+    //         // or should we just use the col widths as data anyway?
             
-            // swap all col-* classes for %ge widths and drop col widths into data objects
-            // or should we just use the col widths as data anyway?
-            
-            // highlight grabbed element?
+    //         // highlight grabbed element?
 
-        },
-        stop: function(event, ui) {
-            // convert data back?
-        },
-        resize: function(event, ui) {
+    //     },
+    //     stop: function(event, ui) {
+    //         // convert data back?
+    //     },
+    //     resize: function(event, ui) {
            
-            // this fires on grid snap (not every mouse pixel) so we can use it to grab the next or previous element
-            // and reszie that too?
+    //         // this fires on grid snap (not every mouse pixel) so we can use it to grab the next or previous element
+    //         // and reszie that too?
 
-            console.log(ui.size);
-        }
-    });
+    //         console.log(ui.size.width + " :: " + $(ui.element).parents('.items').width());
+
+    //         // need to check if the next element 
+
+            
+    //     }
+    // });
 </script>
 @endpush
 
--- resources/views/stack/block/row/item.blade.php
+++ resources/views/stack/block/row/item.blade.php
@@ -1,6 +1,6 @@
-<div class="blockitem xcol-6" style="width: 100%; ">
+<div class="blockitem xcol-{{ $value->cols->width ?? 0 /* width to fill gap... */ }}" style="width: {{ (100 / 12) * $value->cols->width }}%; ">
 
-    <div class="blockitem-content">
+    <div class="blockitem-content" style="margin: 5px; height: 100% ">
 
         <div class="blockitem-handle">
            <select name="{{ $name }}[type]"><option>text</option><option>image</option><option>video</option></select>
@@ -9,6 +9,7 @@
         <div style="display: none">
             Cols = Start: <input type="text" name="{{ $name }}[cols][start]" value="{{ $value->cols->start ?? 1}}" /> Width: <input type="text" name="{{ $name }}[cols][width]" value="{{ $value->cols->width ?? 1 }}" />
         </div>
+
     </div>
 
 </div>
