834a600de41189baa446ad87f86bf176989ba417
Kiersm <kieran@ascent-creative.co.uk>
2021-04-12 22:54:20 +0100

    Block settings panel changes

--- resources/views/stack/block/base/edit.blade.php
+++ resources/views/stack/block/base/edit.blade.php
@@ -11,10 +11,17 @@
 
     <div class="block-settings">
 
-        <div class="controls">
-            <button></button>
+        <div class="controls" style="display: flex; flex-direction: column; align-items: center; padding: 10px 0 10px 10px; font-size: 1.5rem; height:100%;">
+         
+            <div style="flex-basis: 100%; flex-shrink: 1">
+            @yield('block-actions')
+            </div>
+         
+
+            <A href="#" class="block-open-settings bi-gear"></A>
 
             <A href="#" class="block-delete bi-trash"></A>
+
         </div>
 
         <div style="display: none">
