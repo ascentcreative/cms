3955b94e500f2f3333e5c99e03cea24c16539a31
Kiersm <kieran@ascent-creative.co.uk>
2021-04-12 13:56:45 +0100

    Configuring for sizes etc

--- resources/views/stack/block/row/item.blade.php
+++ resources/views/stack/block/row/item.blade.php
@@ -1,13 +1,13 @@
-<div class="blockitem xcol-{{ $value->cols->width ?? 0 /* width to fill gap... */ }}" style="width: {{ (100 / 12) * $value->cols->width }}%; ">
+<div class="blockitem" style="width: {{ (100 / 12) * ($value->cols->width ?? 12) }}%; ">
 
-    <div class="blockitem-content" style="margin: 5px; height: 100% ">
+    <div class="blockitem-content" style="margin: 5px; xheight: 100% ">
 
         <div class="blockitem-handle">
            <select name="{{ $name }}[type]"><option>text</option><option>image</option><option>video</option></select>
         </div>
         <x-cms-form-wysiwyg label="TESTING" name="{{ $name }}[content]" value="{!! isset($value->content) ? $value->content : ''  !!}" wrapper="none"/>
         <div style="display: none">
-            Cols = Start: <input type="text" name="{{ $name }}[cols][start]" value="{{ $value->cols->start ?? 1}}" /> Width: <input type="text" name="{{ $name }}[cols][width]" value="{{ $value->cols->width ?? 1 }}" />
+            Cols = Start: <input type="text" name="{{ $name }}[cols][start]" class="item-col-start" value="{{ $value->cols->start ?? 1}}" /> Width: <input type="text" class="item-col-count" name="{{ $name }}[cols][width]" value="{{ $value->cols->width ?? 12 }}" />
         </div>
 
     </div>
