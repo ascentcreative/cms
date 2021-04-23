a988002ae1f788a484221c51a6628237abf3c75a
Kiersm <kieran@ascent-creative.co.uk>
2021-04-23 11:02:59 +0100

    Correcting value / checkedValue logic

--- resources/views/components/form/checkbox.blade.php
+++ resources/views/components/form/checkbox.blade.php
@@ -1,13 +1,23 @@
 @extends('cms::components.form.wrapper.' . $wrapper)
 
-@section('label')@overwrite
+@section('label'){{$label}}@overwrite
 @section('name'){{$name}}@overwrite
 
 @section('element')
 
-    <label for="{{$name}}" class="flex-label">
-        <input type="checkbox" id="{{$name}}" name="{{$name}}" value="{{$value}}" {{ old('emailsignup') == 1 ? 'checked' : '' }}/>
-        <div class="flex-label-text">@if($labelescape){{$label}}@else{!! $label !!}@endif</div>
-    </label>
+    {{-- @dump($value)
+    @dump($checkedValue)
+    @dump($value == $checkedValue) --}}
+
+    <div style="display: flex; align-items: center; height: 100%;">
+        <input type="checkbox" id="{{$name}}" name="{{$name}}" value="{{$checkedValue}}"
+        
+        @if($value == $checkedValue)
+        checked
+        @endif
+        
+        />
+        {{-- <div class="flex-label-text">@if($labelescape){{$label}}@else{!! $label !!}@endif</div> --}}
+    </div>
 
 @overwrite
