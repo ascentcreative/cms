767231fd5e476c541944cf5b3473bc3a4b10ee49
Kiersm <kieran@ascent-creative.co.uk>
2021-05-03 21:51:45 +0100

    Adding settings to global blade

--- resources/views/contentstack/base.blade.php
+++ resources/views/contentstack/base.blade.php
@@ -1,1 +1,6 @@
-@yield('block')
+
+<div style="@isset($data->bgcolor) background-color: {{ $data->bgcolor }};@endisset @isset($data->bgimage) background-size: cover; background-image: url('{{ $data->bgimage }}') @endisset">
+
+    @yield('block')
+
+</div>
