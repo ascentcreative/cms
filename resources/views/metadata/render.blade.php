b7bbfd59602c6601130d11e37336c54ddd342286
Kiersm <kieran@ascent-creative.co.uk>
2021-05-11 15:04:46 +0100

    Metadata update

--- resources/views/metadata/render.blade.php
+++ resources/views/metadata/render.blade.php
@@ -8,6 +8,6 @@
 <meta name="description" content="{{ $desc }}" />
 
 <meta name="og:description" content="{{ $desc }}" />
-
+<meta name="og:updated_time" content="{{ $model ? $model->updated_at->toAtomString() : '' }}" />
 
 <meta name="twitter:description" content="{{ $desc }}" />
