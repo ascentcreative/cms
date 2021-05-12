e56e225107389914d626fbd5bc6a04644012e2d7
Kiersm <kieran@ascent-creative.co.uk>
2021-05-12 14:09:20 +0100

    Nullable cols on metadata table

---
+++ database/migrations/2021_05_12_135216_metadata_nullables.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class MetadataNullables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('cms_metadata', function (Blueprint $table) {
+            $table->text('keywords')->nullable()->change();
+            $table->text('description')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
