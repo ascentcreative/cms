578e3b06102c4fb9a984d71465a492554bfd2119
Kiersm <kieran@ascent-creative.co.uk>
2021-05-17 20:42:58 +0100

    page_wrapper_blade

--- config/cms.php
+++ config/cms.php
@@ -5,6 +5,7 @@ return [
     'wrapper_blade' => env('CMS_WRAPPER_BLADE', 'base'),
     'wrapper_blade_section' => env('CMS_WRAPPER_BLADE_SECTION', 'pagebody'),
  
+    'page_wrapper_blade' => env('CMS_PAGE_WRAPPER_BLADE', 'cms.page.show'),
 
     // list of paths to try for Model Extender trait blades. Will be checked in the order specified.
     'traitbladepaths' => ['admin.trait', 'cms::trait'],
