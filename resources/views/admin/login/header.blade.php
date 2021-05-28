47a79e442e9283f566a83df9ab6decac0be2b6c6
Kiersm <kieran@ascent-creative.co.uk>
2021-05-28 09:38:28 +0100

    Default Login Logo

--- resources/views/admin/login/header.blade.php
+++ resources/views/admin/login/header.blade.php
@@ -1,1 +1,2 @@
-<img src="/vendor/ascent/cms/img/ascent-badge-trans.png" alt="Ascent" />
+<img src="/vendor/ascent/cms/img/ascent-badge-trans.png" alt="Ascent" height="75" width="75"/>
+<div class="font-weight-bold pt-2">Ascent CMS v3.0</div>
