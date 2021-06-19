637c6cb9b638a150aa163e181bbd0ebfc9fc98f7
Kiersm <kieran@ascent-creative.co.uk>
2021-06-19 18:00:17 +0100

    Site Settings Page

--- resources/views/admin/settings/edit.blade.php
+++ resources/views/admin/settings/edit.blade.php
@@ -1,5 +1,7 @@
 @extends('cms::admin.base.edit')
 
+@section('screentitle', 'Site Settings')
+
 @section('screen-start')
         
     {{-- OPEN FORM TAG --}}
@@ -37,23 +39,82 @@
 
 
 @section('editform')
+    <div class="cms-screenblock-tabs bg-white rounded shadow" style="">
 
-    <div class="cms-screenblock cms-screenblock-main bg-white rounded shadow" style="">
+    {{--  <div class="cms-screenblock cms-screenblock-main bg-white rounded shadow" style=""> --}}
+
+        <ul class="nav nav-tabs px-3 pt-3 bg-light" id="myTab" role="tablist">
+            <li class="nav-item">
+              <a class="nav-link" id="main-tab" data-toggle="tab" href="#main" role="tab" aria-controls="main" aria-selected="true">Main Site Settings</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact Form</a>
+              </li>
+            {{-- <li class="nav-item">
+              <a class="nav-link" id="menuitem-tab" data-toggle="tab" href="#menuitem" role="tab" aria-controls="menuitem" aria-selected="false">Menu Position</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" id="metadata-tab" data-toggle="tab" href="#metadata" role="tab" aria-controls="metadata" aria-selected="false">Metadata</a>
+            </li> --}}
+        </ul>
     
-        <x-cms-form-input type="text" name="site_name" label="Site Name" value="{{ old('site_name', $model->site_name) }}">
+        <div class="tab-content" id="myTabContent">
+            <div class="tab-pane show p-3" id="main" role="tabpanel" aria-labelledby="main-tab">
+                <x-cms-form-input type="text" name="site_name" label="Site Name" value="{{ old('site_name', $model->site_name) }}">
+                    Shows in the tab label for all pages of the site
+                </x-cms-form-input>
+        
+        
+                <x-cms-form-textarea type="text" name="custom_head_tags" label="Custom HEAD Tags" value="{!! old('custom_head_tags', $model->custom_head_tags) !!}">
+                   Adds this code to the HTML Head of the site. Best used for custom Meta tags (Facebook pixel verification etc).<br/><strong>Do not use for anything which would need to be covered by cookie opt-in code (i.e. analytics / tracking coode etc)</strong>
+                </x-cms-form-textarea>
+            </div>
+
+
+            {{-- CONTACT FORM SETTINGS --}}
+            <div class="tab-pane show p-3" id="contact" role="tabpanel" aria-labelledby="contact-tab">
+
+                <x-cms-form-input type="text" name="contact_to_addresses" label="Send To" value="{{ old('contact_to_addresses', $model->contact_to_addresses) }}">
+                    The email address(es) which should recieve contact form submissions - separate with commas.
+                </x-cms-form-input>
+
+
+                
+                <x-cms-form-input type="text" name="contact_from_name" label="From Name" value="{{ old('contact_from_name', $model->contact_from_name) }}">
+                    The name you'll receive contact form submissions from.
+                </x-cms-form-input>
+
+                <x-cms-form-input type="text" name="contact_from_address" label="From Address" value="{{ old('contact_from_address', $model->contact_from_address) }}">
+                    The name you'll receive contact form submissions from.
+                </x-cms-form-input>
+
+
+
+                @if(config('cms.recaptcha_secret'))
+                <x-cms-form-input type="text" name="contact_recaptcha_threshold" label="Google Recaptcha Threshold" value="{{ old('contact_recaptcha_threshold', $model->contact_recaptcha_threshold) }}">
+                    The score below which enquiries will be rejected as spam.
+                </x-cms-form-input>
+                @endif
+        
+        
+              
+            </div>
+
+
+        </div>
+
+      
+
+
+        {{-- <x-cms-form-input type="text" name="test[]" label="Test 1" :value="$model->test[0]">
             Shows in the tab label for all pages of the site
         </x-cms-form-input>
 
-    </div>
-
-    <div class="cms-screenblock bg-white rounded shadow" style="">
-
-        {{-- <x-cms-form-textarea name="value" label="Value(s)" value="{{ old('value', '') }}"/> --}}
-
-        
-
-
+        <x-cms-form-input type="text" name="test[]" label="Test 2" :value="$model->test[1]">
+            Shows in the tab label for all pages of the site
+        </x-cms-form-input> --}}
 
     </div>
+
 
 @endsection
