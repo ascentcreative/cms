772663188a9ffae8495bbba0f2ab717e85199276
Kiersm <kieran@ascent-creative.co.uk>
2021-06-29 14:34:38 +0100

    Value treated as PHP object

--- resources/views/trait/publishable.blade.php
+++ resources/views/trait/publishable.blade.php
@@ -8,26 +8,20 @@
 <div class="border p-2 ml-5" style="flex-basis: 300px;">
     
     <div class="p-2">
-    {{-- <div class="col-4"> --}}
         <x-cms-form-checkbox type="" name="publishable" label="Published?" checkedValue="1" uncheckedValue="0" value="{{ old('publishable', $model->publishable) }}" wrapper="inline"></x-cms-form-checkbox>
-    {{-- </div> --}}
     </div>
 
     <div class="p-2">
-    {{-- <div class="col-4"> --}} 
         <x-cms-form-datetime name="publish_start" label="Publish At" :value="old('publish_start', $model->publish_start)" wrapper="inline">
             (leave blank to publish immediately)
         </x-cms-form-input>
-    {{-- </div> --}}
     </div>
 
 
     <div class="p-2">
-    {{-- <div class="col-4"> --}}
         <x-cms-form-datetime name="publish_end" label="Published Until" :value="old('publish_end', $model->publish_end)" wrapper="inline">
             (leave blank to publish indefinitely)
         </x-cms-form-datetime>
-    {{-- </div> --}}
     </div>
 
 </div>
