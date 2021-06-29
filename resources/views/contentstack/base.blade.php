c0311abe45e6ea33adb34bad0479ba5ddf170253
Kiersm <kieran@ascent-creative.co.uk>
2021-06-29 14:33:33 +0100

    Block IDs for targetted CSS

--- resources/views/contentstack/base.blade.php
+++ resources/views/contentstack/base.blade.php
@@ -1,5 +1,4 @@
-
-<div style="@isset($data->bgcolor) background-color: {{ $data->bgcolor }};@endisset @isset($data->bgimage) background-size: cover; background-image: url('{{ $data->bgimage }}') @endisset">
+<div id="block_{{ $block->slug }}" style="@isset($data->bgcolor) background-color: {{ $data->bgcolor }};@endisset @isset($data->bgimage) background-size: cover; background-image: url('{{ $data->bgimage }}') @endisset">
 
     @yield('block')
 
