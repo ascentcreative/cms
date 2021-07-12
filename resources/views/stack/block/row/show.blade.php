c72153402a38088b7e0e0bba213533be47b3151f
Kiersm <kieran@ascent-creative.co.uk>
2021-07-12 20:59:50 +0100

    Null data handling

--- resources/views/stack/block/row/show.blade.php
+++ resources/views/stack/block/row/show.blade.php
@@ -1,38 +1,28 @@
 {{-- @dump($data) --}}
 
 <div class="row" style="padding: 0px 0 20px">
-@foreach($data->items as $item) 
+@isset($data->items)
+    @foreach($data->items as $item) 
 
-    <div class="col-md-{{$item->cols->width}} xcol-sm-{{$item->cols->width * 2}} @if($item->cols->width < 6) hyphenbreak @endif" style="xpadding-bottom: 20px; xwhite-space: pre-wrap; xtab-size: 10;">
+        <div class="col-md-{{$item->cols->width}} xcol-sm-{{$item->cols->width * 2}} @if($item->cols->width < 6) hyphenbreak @endif" style="xpadding-bottom: 20px; xwhite-space: pre-wrap; xtab-size: 10;">
 
-        @switch($item->type)
-            @case('text')
-                {!! $item->content !!}
-                @break
+            @switch($item->type)
+                @case('text')
+                    {!! $item->content !!}
+                    @break
 
-            @case('image')
-                <IMG src="{{ $item->image }}" width="100%"/>
-                @break
+                @case('image')
+                    <IMG src="{{ $item->image }}" width="100%"/>
+                    @break
 
-            @case('video')
-                {{ embedVideo('https://youtu.be/VkDL6nFPHKU') }}
-                @break
+                @case('video')
+                    {{ embedVideo('https://youtu.be/VkDL6nFPHKU') }}
+                    @break
 
-        @endswitch
+            @endswitch
 
-    </div>
+        </div>
 
-
-@endforeach
+    @endforeach
+@endisset
 </div>
-
-
-{{-- -ms-word-break: break-all;
-     word-break: break-all;
-
-     /* Non standard for WebKit */
-     word-break: break-word;
-
--webkit-hyphens: auto;
-   -moz-hyphens: auto;
-        hyphens: auto; --}}
--- resources/views/stack/render.blade.php
+++ resources/views/stack/render.blade.php
@@ -1,9 +1,10 @@
 {{-- {!! $content !!} --}}
 
 
+@isset($content)
+    @foreach(json_decode($content) as $item)
 
-@foreach(json_decode($content) as $item)
+        @includeFirst(['stack.block.' . $item->type . '.show', 'cms::stack.block.' . $item->type . '.show'], ['data'=>$item])
 
-    @includeFirst(['stack.block.' . $item->type . '.show', 'cms::stack.block.' . $item->type . '.show'], ['data'=>$item])
-
-@endforeach
+    @endforeach
+@endisset
