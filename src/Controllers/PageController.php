c2e07e46d46f49c909910627535c17ea8fafce51
Kiersm <kieran@ascent-creative.co.uk>
2021-07-30 15:40:18 +0100

    Enable Previews (temp fix)

--- src/Controllers/PageController.php
+++ src/Controllers/PageController.php
@@ -11,11 +11,11 @@ class PageController extends Controller
 {
 
     /* even though the page model is defined here, the bindings swap the class at runtime */
-    public function show(\AscentCreative\CMS\Models\Page $page) {
+    public function show(\AscentCreative\CMS\Models\Page $page, $forceHTML=false ) {
 
         headTitle()->add($page->title);
 
-        if (request()->wantsJson()) {
+        if (request()->wantsJson() && !$forceHTML) {
             return view('cms::public.pages.modal')->withModel($page);
         } else {
             return view('cms::public.pages.show')->withModel($page);
