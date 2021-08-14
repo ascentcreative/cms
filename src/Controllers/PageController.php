ab2bf0471813c256e0c8dcf325250c7c847c1f32
Kiersm <kieran@ascent-creative.co.uk>
2021-08-14 18:51:49 +0100

    Don't add page title on homepage route

--- src/Controllers/PageController.php
+++ src/Controllers/PageController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
  
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Route;
 
 class PageController extends Controller
 {
@@ -13,8 +14,10 @@ class PageController extends Controller
     /* even though the page model is defined here, the bindings swap the class at runtime */
     public function show(\AscentCreative\CMS\Models\Page $page, $forceHTML=false ) {
 
-        headTitle()->add($page->title);
-
+        if(Route::currentRouteName() != 'homepage') {
+            headTitle()->add($page->title);
+        }
+        
         if (request()->wantsJson() && !$forceHTML) {
             return view('cms::public.pages.modal')->withModel($page);
         } else {
