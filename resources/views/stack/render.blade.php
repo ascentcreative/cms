1d43a1572b8ae1261929fe7d20c721e6451557f2
Kiersm <kieran@ascent-creative.co.uk>
2021-09-05 15:07:01 +0100

    Change BG colour to overlaid div

--- resources/views/stack/render.blade.php
+++ resources/views/stack/render.blade.php
@@ -10,9 +10,9 @@
 
             {{-- This needs to use some form of auto conversion... ie put everythig in a styles property to automatically write to the tag  --}}
            {{--  will also need to know how to format values (although these could be selected on the setting themselves) --}}
-            style="@if(isset($item->bgcolor) && $item->bgcolor != '') 
+            style="position: relative; {{-- @if(isset($item->bgcolor) && $item->bgcolor != '') 
                     background-color: {{ $item->bgcolor }};
-                @endif @if(isset($item->padding->top) && $item->padding->top != 0) 
+                @endif --}} @if(isset($item->padding->top) && $item->padding->top != 0) 
                     padding-top: {{ $item->padding->top }}px;
                 @endif  @if(isset($item->padding->bottom) && $item->padding->bottom != 0) 
                     padding-bottom: {{ $item->padding->bottom }}px;
@@ -29,6 +29,7 @@
                 @endif @if(isset($item->margin->right) && $item->margin->right != 0) 
                     margin-right: {{ $item->margin->right }}px;
                 @endif @if(Agent::isMobile() && isset($item->bgimage) && $item->bgimage != '')
+                    {{-- Can't use parallax on mobile, so add the image as a normal backrgound --}}
                     background-image: url('/storage/{{ \AscentCreative\CMS\Models\File::find($item->bgimage)->filepath }}');
                     background-size: cover;
                     background-position: center;
@@ -36,14 +37,17 @@
                 
                 @if(!Agent::isMobile())
                     @if(isset($item->bgimage) && $item->bgimage != '') 
+                        {{-- Parallax image for non-mobile users. --}}
                         data-android-fix="false" class="parallax-window" data-parallax="scroll" data-image-src="/storage/{{ \AscentCreative\CMS\Models\File::find($item->bgimage)->filepath }}"
                     @endif
                 @endif         
 
                 >
 
-      
-
+                {{-- To allow for blended BG colours over images, put the color as an overlaid div. --}}
+                @if(isset($item->bgcolor) && $item->bgcolor != '') 
+                    <div class="bgcolor" style="background-color: {{ $item->bgcolor }}; position: absolute; top: 0; bottom: 0; left: 0; right: 0"></div>
+                @endif
 
             {{-- @if(!isset($item->fullwidth) || !$item->fullwidth)  --}}
                 <div 
