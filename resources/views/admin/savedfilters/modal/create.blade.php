1250222c1e14246a96d09dde18bce1ef315c41aa
Kiersm <kieran@ascent-creative.co.uk>
2021-09-12 22:10:07 +0100

    Description and better alignment

--- resources/views/admin/savedfilters/modal/create.blade.php
+++ resources/views/admin/savedfilters/modal/create.blade.php
@@ -19,11 +19,17 @@ Save Filters
 
             {{ csrf_field() }}  
 
+
+            <div class="bg-light border rounded p-3 mb-3 flex flex-between">
+                This will save all selected filters, sorting options and the number of rows to display, as currently configured on the page.
+             </div>
+
+
             <div class="border p-3 mb-3">
 
                 <x-cms-form-input name="name" value="{{ old('name', '') }}" label="Filter Name" type="text" aria-autocomplete="false" />
 
-                <x-cms-form-checkbox name="is_global" checkedValue="1" uncheckedValue="0" label="Visible to all users?" type="" value=""/>
+                <x-cms-form-checkbox name="is_global" checkedValue="1" uncheckedValue="0" label="Visible to all users?" wrapper="inline" type="" value=""/>
 
             </div>
 
