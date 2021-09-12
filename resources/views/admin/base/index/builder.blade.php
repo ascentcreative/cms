952345b3ae666e01595b5dbbb9ec1901854da34a
Kiersm <kieran@ascent-creative.co.uk>
2021-09-12 18:58:43 +0100

    Major sort / filter / paginate update

--- resources/views/admin/base/index/builder.blade.php
+++ resources/views/admin/base/index/builder.blade.php
@@ -6,15 +6,23 @@
     @foreach($columns as $col) 
 
    
-        <th class="@if($col->align) text-{{$col->align}} @endif " @if(!is_null($col->width)) width="{{ $col->width }}" @endif >
+        <th class="@if($col->align) text-{{$col->align}} @endif @if($col->filterable) filterable @endif" @if(!is_null($col->width)) width="{{ $col->width }}" @endif >
             
+            <div class="flex" style="flex-wrap: nowrap;">
+
             @if($col->sortable)
-                <A href="{{ $col->buildSortUrl() }}" class="sort-link sort-link-{{ $col->sorted }} ">
+               
             @endif
 
-                {{ $col->title }}
+            @if($col->titleBlade)
+                @include($col->titleBlade)
+            @else   
+                <div style="flex-grow: 1">{{ $col->title }}</div>
+            @endif
 
             @if($col->sortable)
+
+                <A href="{{ $col->buildSortUrl() }}" class="sort-link sort-link-{{ $col->sorted }} ml-1"">
 
                 @switch($col->sorted)
                         @case('asc')
@@ -30,11 +38,48 @@
                             @break
 
                 @endswitch
-                
-            
             
                 </A>
+
+                @isset(request()->sort[$col->slug])
+                    {{-- Hidden field to store the sort value so it persists on filter change --}}
+                    <input type="hidden" name="sort[{{ $col->slug }}]" value="{{ request()->sort[$col->slug] }}" />
+                @endisset
+
             @endif
+
+            @if($col->filterable)
+                {{-- <div class="filter-block">
+                    
+                    <div class="filter-ui">
+                        OPTIONS GO HERE...
+                    </div>
+                </div> --}}
+
+                <div class="filter xdropdown xdropdown-no-caret ml-1" style="display: inline-block">
+                    {{-- <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      Dropdown button
+                    </button> --}}
+                    <A href="" class="filter-toggle xdropdown" data-toggle="filter-options" aria-haspopup="true" aria-expanded="false" xclass="badge badge-secondary"><i class="@isset(request()->cfilter[$col->slug]) bi-funnel-fill @else bi-funnel @endisset"></i></A>
+                    <div class="filter-panel xdropdown-menu xdropdown-menu-right" aria-labelledby="xdropdownMenuButton">
+                      
+                        <div class="filter-options font-weight-normal">
+
+                            {{-- <form method="POST" action="{{ $col->buildFilterUrl() }}" class="filter-form" name="cfilter-form-{{ $col->slug }}" id="">  --}}
+                                {{-- {{ csrf_field() }}  --}} 
+                                @include($col->filterBlade, $col->getFilterBladeParameters())
+
+                            {{-- </form> --}}
+
+                        </div>
+
+                    </div>
+                  </div>
+
+            @endif
+
+            </div>
+
 
         </th>
 
@@ -82,3 +127,90 @@
 @endforeach
 
 @endsection
+
+
+@section('paginator')
+
+        {{-- <div class="cms-screen-paginator">
+                
+                <A href="{{ $models->previousPageUrl() }}"> Prev </A> | <A href="{{ $models->nextPageUrl() }}"> Next </A>
+
+            </div> --}}
+
+            <div class="flex flex-between flex-align-center mt-3 border-top pt-3">
+
+                <div><small>Showing {{ $models->firstItem() }}-{{ $models->lastItem() }} of {{ $models->total() }} {{ $modelPlural }}</small></div>
+    
+                @if($models instanceof \Illuminate\Pagination\LengthAwarePaginator && $models->lastPage() > 1 )
+                <div class="small">{{ $models->links( 'cms::admin.pagination.bootstrap-4' ) }} </div>
+                @endif
+    
+    
+                <div class="flex flex-center">
+                    
+                    <div class="small">Show&nbsp;</div>
+                    <div class="small">
+                        <select class="form-control form-control-sm" name="pageSize" id="pageSize">
+                            <option value="15" @if(request()->pageSize == 15) selected @endif>15</option>
+                            <option value="25" @if(request()->pageSize == 25) selected @endif>25</option>
+                            <option value="50" @if(request()->pageSize == 50) selected @endif>50</option>
+                            <option value="100" @if(request()->pageSize == 100) selected @endif>100</option>
+                            <option value="500" @if(request()->pageSize == 500) selected @endif>500</option>
+                            <option value="1000" @if(request()->pageSize == 1000) selected @endif>1000</option>
+                            {{-- <option value="all">All</option> --}}
+                        </select>
+                    </div>
+                    <div class="small">&nbsp; Rows</div>
+                </div>
+    
+            </div>
+    
+
+@endsection
+
+
+
+@push('scripts')
+
+<script>
+
+    $('form#frm-indexparams .filter, #pageSize').on('change', function(e) {
+        //console.log(e.target);
+        $('form#frm-indexparams').submit();
+    });
+
+    $('.filter-panel').on('click', function(e) {
+        e.stopPropagation();
+    });
+
+    $('.filter').on('click', '.filter-toggle', function(e) {
+        e.preventDefault();
+      
+        console.log(this);
+
+        $(this).parents('.filter').find('.filter-panel').slideDown(100, function() {
+           
+            panel = this;
+
+            $('body').on('click', function(e) {
+                
+              //  console.log(e.target);
+                $(panel).slideUp(100, function() {
+                    
+                });
+
+                $('body').unbind('click');
+                 //, this);
+            });
+
+        });
+
+        
+
+       // e.stopPropagation();
+
+    });
+
+</script>
+
+@endpush
