3c5c9def64fe9ed9f68f347278149521f0f84a0f
Kiersm <kieran@ascent-creative.co.uk>
2021-09-12 20:42:31 +0100

    Create

---
+++ resources/views/admin/ui/index/checkalltoggle.blade.php
@@ -0,0 +1,23 @@
+<a href="{{ url()->current() }}" id="btn-select-toggle" class="bi-ui-checks text-dark" style="font-size: 1.2rem; line-height: 1em;" title="Check / Uncheck All"></a>
+
+@push('scripts')
+
+<script>
+
+    $('A#btn-select-toggle').click(function(e) {
+        e.preventDefault();
+        if ( $('INPUT.item-select').not(':checked').length == 0 ) {
+            // uncheck all
+            $('INPUT.item-select').prop('checked', false);
+            $('A#btn-select-toggle').removeClass('text-primary').addClass('text-dark');
+        } else {
+            // check all
+            $('INPUT.item-select').prop('checked', true);
+            $('A#btn-select-toggle').removeClass('text-dark').addClass('text-primary');
+        }
+
+    });
+
+</script>
+
+@endpush
