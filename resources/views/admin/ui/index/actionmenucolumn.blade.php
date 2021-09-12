5bde6014bf5f42781250a8a801b6533b936a94b0
Kiersm <kieran@ascent-creative.co.uk>
2021-09-12 20:42:22 +0100

    Make overridable

--- resources/views/admin/ui/index/actionmenucolumn.blade.php
+++ resources/views/admin/ui/index/actionmenucolumn.blade.php
@@ -1,6 +1,10 @@
 <div class="btn-group dropleft">
     <A class="dropdown-toggle dropdown-toggle-dots" href="#" data-toggle="dropdown" ></A>
     <div class="dropdown-menu dropdown-menu-right" style="">
-        <a class="dropdown-item text-sm btn-delete modal-link" href="{{ action([controller(), 'delete'], [$modelInject => $item->id]) }}">Delete</a> 
+        
+        @section('action-menu-buttons')
+            <a class="dropdown-item text-sm btn-delete modal-link" href="{{ action([controller(), 'delete'], [$modelInject => $item->id]) }}">Delete {{ $modelName }}</a> 
+        @show
+
     </div>
 </div>
