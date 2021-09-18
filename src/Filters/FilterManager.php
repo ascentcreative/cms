4a35848d24d027910bbb9333f3bbfe6064bcc92d
Kiersm <kieran@ascent-creative.co.uk>
2021-09-18 19:40:05 +0100

    Filter Manager Dev

---
+++ src/Filters/FilterManager.php
@@ -0,0 +1,105 @@
+<?php
+namespace AscentCreative\CMS\Filters;
+
+class FilterManager {
+
+
+    // need to somehow register the filters and sorters
+    // I'm thinking by fieldname in the request data and the scope name
+    
+    // should we also grab a wrapper field name?
+    // i.e. cfilters[themes] => wrapper[field]
+
+    // allow a global wrapper to be set, but each individual registration might override it?
+
+    public $filter_wrapper = 'filter';
+    public $sorter_wrapper = 'sort';
+
+    private $filters = [];
+    private $sorters = [];
+
+
+    public function setFilterWrapper($wrapper) {
+        $this->filter_wrapper = $wrapper;
+        return $this;
+    }
+
+    public function setSorterWrapper($wrapper) {
+        $this->sorter_wrapper = $wrapper;
+        return $this;
+    }
+
+
+    /**
+     * Register a new filter
+     * param: $field - the name of the field
+     * param: $socpe - the eloquent scope name to apply on the model - i.e. scopeByTheme = 'byTheme';
+     * param: $wrapper - override the wrapper for this field
+     */
+    public function registerFilter($field, $scope, $wrapper=null) {
+
+        $this->filters[$field] = $scope;
+
+        return $this;
+
+    }
+
+    /**
+     * Register a new sorter
+     * param: $field - the name of the field
+     * param: $socpe - the eloquent scope name to apply on the model - i.e. scopeByTheme = 'byTheme';
+     * param: $wrapper - override the wrapper for this field
+     */
+    public function registerSorter($field, $scope, $wrapper=null) {
+ 
+        $this->filters[$key] = $scope;
+
+        return $this;
+
+    }
+
+
+    public function apply($query, $data=null) {
+
+        $data = $data ?? request()->all();
+
+        // dd($data);
+
+        $wrapper = $this->filter_wrapper;
+        if ($wrapper == '') {
+            $filter_data = $data;
+        } else {
+            $filter_data = $data[$wrapper] ?? [];
+        }
+
+        foreach($this->filters as $key=>$scope) {
+            if(isset($filter_data[$key])) {
+
+                // eager load for performance?
+                //$query->with($key);
+
+                $query->$scope($filter_data[$key]);
+            }
+        }
+
+        // does this need to police only one sorter?
+
+        $wrapper = $this->sorter_wrapper;
+        if ($wrapper == '') {
+            $sorter_data = $data;
+        } else {
+            $sorter_data = $data[$wrapper] ?? [];
+        }
+
+        foreach($this->sorters as $key=>$scope) {
+            if(isset($sorter_data[$key])) {
+                $query->$scope($sorter_data[$key]);
+            }
+        }
+
+        return $query;
+
+    }
+
+
+}
