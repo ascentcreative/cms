9bc2b0d5d1977d7f461f8acee31c55fc4db50262
Kiersm <kieran@ascent-creative.co.uk>
2021-09-21 17:04:55 +0100

    Missing colon

--- resources/views/components/form/wrapper/simple.blade.php
+++ resources/views/components/form/wrapper/simple.blade.php
@@ -1,7 +1,7 @@
 <div class="simple-form-element element-wrapper {{$class}}" id="{{$name}}-wrapper">
 
     @hasSection('label')
-        <label for="@yield('name')" class="simple-form-element-label">@yield('label')</label>
+        <label for="@yield('name')" class="simple-form-element-label">@yield('label'):</label>
     @endif
 
     @yield('element')
