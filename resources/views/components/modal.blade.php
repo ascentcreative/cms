8880c9c2ea9e8f91f983adc46c35c1355af28579
Kiersm <kieran@ascent-creative.co.uk>
2021-10-18 17:17:58 +0100

    Footer handling

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -36,7 +36,7 @@
             <button type="submit" class="btn btn-danger"> Yes </button>
          --}}
         
-                {{ $footer }}
+                {{ $footer ?? '' }}
 
           </div>
         {{-- @endif --}}
