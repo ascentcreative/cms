0c159ad9ca7ea77af3d872222792f3bf9f559841
Kiersm <kieran@ascent-creative.co.uk>
2022-01-02 14:06:55 +0000

    MultiSizeImage display component

---
+++ src/View/Components/Display/MultiSizeImage.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace AscentCreative\CMS\View\Components\Display;
+
+use Illuminate\View\Component;
+
+class MultiSizeImage extends Component
+{
+
+    public $src;
+    public $alt;
+
+    // these are populated from the files discovered on disk.
+    public $srcset;
+    public $sizes;
+
+  
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct($src, $alt)
+    {
+        $this->src = $src;
+        $this->alt = $alt;
+
+
+        /** Dynamically calculate the sizes and srcset from the stored images 
+         *  (Note - doesn't use the sizes from the config file in case they've changed since the files were created )
+         */
+
+        $info = pathinfo($src);
+
+        $arySizes = [];
+
+        $srcset = [];
+        $sizes = [];
+    
+        $globname = $_SERVER['DOCUMENT_ROOT'] . $info['dirname'] . '/' . $info['filename'] . '@*.' . $info['extension'];
+        foreach(glob($globname) as $file) {
+            $image_width = getimagesize($file)[0];
+            $srcset[] = str_replace($_SERVER['DOCUMENT_ROOT'], '', $file) . ' ' . $image_width . 'w';
+            $sizes[] = $image_width; 
+        } 
+
+        $sizelist = [];
+        $lastsize = '';
+
+        foreach(collect($sizes)->sortDesc()->toArray() as $size) {
+            $sizelist[] = $lastsize . $size . 'px';
+            $lastsize = "(max-width: " . $size . "px) ";
+        }
+
+        $this->srcset = join(', ', $srcset);
+        $this->sizes = join(', ', array_reverse($sizelist));
+        
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|string
+     */
+    public function render()
+    {
+        return view('cms::components.display.multisizeimage');
+    }
+}
