ed890d1a70dfd65b9a8875d31ead577ea2eb4b06
Kiersm <kieran@ascent-creative.co.uk>
2022-01-03 11:23:18 +0000

    Extra Attributes

--- resources/views/components/display/multisizeimage.blade.php
+++ resources/views/components/display/multisizeimage.blade.php
@@ -2,4 +2,9 @@
     {{-- src="{{ $src }}" --}}
     srcset="{{ $srcset }}"
     sizes="{{ $sizes }}"
-    alt ="{{ $alt }}" />
+    alt="{{ $alt }}" 
+    @if($class) class="{{ $class }}" @endif
+    @if($style) style="{{ $style }}" @endif
+    @if($width) width="{{ $width }}" @endif
+    @if($height) height="{{ $height }}" @endif
+    />
