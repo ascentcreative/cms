6d6e9fb16d1d1987aa638f6d62a8a3db40943897
Kiersm <kieran@ascent-creative.co.uk>
2022-01-06 22:34:03 +0000

    Admin Forgotten Password Views

---
+++ resources/views/admin/auth/forgot-password.blade.php
@@ -0,0 +1,51 @@
+@extends('cms::admin.base')
+
+@section('pagebody')
+
+<div class="flex flex-center flex-align-center" style="height: 80vh; flex-direction: column">
+
+    <div style="width: 200px; margin: 20px; text-align: center">
+    @includeFirst(['admin.login.header', 'cms::admin.login.header'])
+    </div>
+
+   
+    <div class="formpanel border-top" style="width: 400px">
+
+
+     <div class="p-2 mt-3 mb-3 border rounded bg-white font-medium text-sm" style="width: 400px;">
+        @if (session('status'))
+       
+           {!! session('status') !!}
+       @else
+           Enter your email address to receive a password reset email.
+       @endif
+       </div>
+
+    </div>
+
+    <div class="formpanel border-top" style="width: 400px">
+
+
+        <form action="/forgot-password" method="POST">
+
+            @csrf
+
+            <div class="pt-3">
+            <x-cms-form-input type="text" name="email" label="Email" value="{{ old('email','') }}" wrapper="simple"/>
+            </div>
+
+             {{-- <x-cms-form-input type="text" name="email" label="Email" value="{{ old('email','') }}" wrapper="simple"/> --}}
+   
+
+            <div class="text-center p-4">
+                <x-cms-form-button name="reset" label="Reset Password" value="" wrapper="none" class="button btn-primary btn"/>
+            </div>
+
+        </form>
+
+        
+    </div>
+
+
+
+@endsection
---
+++ resources/views/admin/auth/reset-password.blade.php
@@ -0,0 +1,101 @@
+@extends('cms::admin.base')
+
+@section('pagebody')
+
+
+<div class="flex flex-center flex-align-center" style="height: 80vh; flex-direction: column">
+
+    <div style="width: 200px; margin: 20px; text-align: center">
+    @includeFirst(['admin.login.header', 'cms::admin.login.header'])
+    </div>
+
+   
+    <div class="formpanel border-top" style="width: 400px">
+
+
+     <div class="p-2 mt-3 mb-3 border rounded bg-white font-medium text-sm" style="width: 400px;">
+        @if (session('status'))
+       
+           {!! session('status') !!}
+       @else
+           Check your email address is correct, and enter a new password below
+       @endif
+       </div>
+
+    </div>
+
+    <div class="formpanel border-top" style="width: 400px">
+
+        <form action="/reset-password" method="POST">
+            @csrf
+            
+            <div class="pt-3">
+                <x-cms-form-input type="text" name="email" label="Email" value="{{ old('email', request()->email ) }}" wrapper="simple"/>
+            </div>
+
+            <div class="pt-3">
+           <x-cms-form-input type="password" name="password" label="Password" value="{{ old('password','') }}"  wrapper="simple"/>
+        </div>
+
+        <div class="pt-3">
+           <x-cms-form-input type="password" name="password_confirmation" label="Confirm password" value="{{ old('password_confirmation','') }}"  wrapper="simple"/>
+              
+        </div>
+           
+            <div class="text-center p-4">
+                <x-cms-form-button name="reset" label="Reset Password" value="" wrapper="none" class="button btn-primary btn"/>
+            </div>
+
+            <input type="hidden" name="token" value="{{request()->route('token')}}" />
+   
+
+            @if (session('status'))
+               <div class="mb-4 font-medium text-sm text-green-600">
+                   {!! session('status') !!}
+               </div>
+            @endif
+            
+            </form>
+
+    </div>
+       
+
+
+{{-- 
+    <div class="flex flex-center flex-align-center" style="height: 80vh; flex-direction: column">
+
+<H1 class="text-center w-100">Reset Your Password</H1>
+
+
+
+  <div class="" style="display: flex; justify-content: center; width: 100%;">   
+      
+    <div class="formpanel">
+     <form action="/reset-password" method="POST">
+         @csrf
+         
+         <x-cms-form-input type="text" name="email" label="Email" value="{{ old('email', request()->email ) }}" wrapper="simple"/>
+
+        <x-cms-form-input type="password" name="password" label="Password" value="{{ old('password','') }}"  wrapper="simple"/>
+        <x-cms-form-input type="password" name="password_confirmation" label="Confirm password" value="{{ old('password_confirmation','') }}"  wrapper="simple"/>
+           
+        <input type="hidden" name="token" value="{{request()->route('token')}}" />
+
+         <x-cms-form-button name="reset" label="Reset Password" value="" wrapper="simple"/>
+         
+         @if (session('status'))
+            <div class="mb-4 font-medium text-sm text-green-600">
+                {!! session('status') !!}
+            </div>
+         @endif
+         
+         </form>
+
+    </div>
+
+</div>
+
+    </div> --}}
+
+
+@endsection
