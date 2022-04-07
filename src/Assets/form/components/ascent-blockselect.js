67d006fc16f053e188caaff3a2956eac6cbcd646
Kiersm <kieran@ascent-creative.co.uk>
2022-04-07 23:09:16 +0100

    Prevent double handling of chaneg event

--- src/Assets/form/components/ascent-blockselect.js
+++ src/Assets/form/components/ascent-blockselect.js
@@ -10,30 +10,12 @@ var BlockSelect = {
 
     _init: function () {
 
+       
         var self = this;
         self.trackOrder = [];
         
-        $(this.element).on('change', '.cms-blockselect-option INPUT', function() {
-
-            if($(this).is(':checked')) {
-                
-                $(this).parents('.cms-blockselect-option').addClass('selected');
-
-                self.trackOrder.push($(this).attr('id'));
-
-                if(self.element.data('max-select') != -1 && self.trackOrder.length > self.element.data('max-select')) {
-                    elm = self.trackOrder.shift();
-                    $('.cms-blockselect-option INPUT#' + elm).prop('checked', false).parents('.cms-blockselect-option').removeClass('selected');
-                }
-
-            } else {
-               
-                $(this).parents('.cms-blockselect-option').removeClass('selected');
-
-                self.trackOrder.splice(self.trackOrder.indexOf($(this).attr('id')), 1);    
-            }
-   
-        });
+        $(this.element).off('change'); 
+        $(this.element).on('change', '.cms-blockselect-option INPUT', {widget: self}, self.processChange);
 
 
         $(this.element).find('INPUT:checked').each(function() {
@@ -51,6 +33,31 @@ var BlockSelect = {
     setOptions: function($opts) {
 
         // replace the existing options with the provided array
+
+    },
+
+    processChange: function(evt) {   
+
+        let self = evt.data.widget;
+
+        if($(this).is(':checked')) {
+
+            
+            $(this).parents('.cms-blockselect-option').addClass('selected');
+
+            self.trackOrder.push($(this).attr('id'));
+
+            if(self.element.data('max-select') != -1 && self.trackOrder.length > self.element.data('max-select')) {
+                elm = self.trackOrder.shift();
+                $('.cms-blockselect-option INPUT#' + elm).prop('checked', false).parents('.cms-blockselect-option').removeClass('selected');
+            }
+
+        } else {
+            
+            $(this).parents('.cms-blockselect-option').removeClass('selected');
+
+            self.trackOrder.splice(self.trackOrder.indexOf($(this).attr('id')), 1);    
+        }
 
     }
 
