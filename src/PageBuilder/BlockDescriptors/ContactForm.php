d18e2ac110c75d6f74fee3ee7f2c030b9d8b72d9
Kieran Metcalfe <kieran@ascent-creative.co.uk>
2022-08-30 20:47:02 +0100

    Pagebuilder Blocks

---
+++ src/PageBuilder/BlockDescriptors/ContactForm.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AscentCreative\CMS\PageBuilder\BlockDescriptors;
+
+use AscentCreative\PageBuilder\BlockDescriptors\AbstractDescriptor; 
+
+class ContactForm extends AbstractDescriptor { 
+
+    public static $name = 'Contact Form';
+
+    public static $bladePath = 'contact-form';
+
+    public static $description = "Inserts a Contact Form";
+
+    public static $category = "Functionality";
+
+}
